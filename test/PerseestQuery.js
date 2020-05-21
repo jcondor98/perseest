2de67df5f31cb3b896954c96dd9e0171d9b62728
jcondor <paololucchesi98@gmail.com>
2020-05-21 15:36:36 +0200

    Implemented pending tests and done minor bugfixes

--- test/PerseestQuery.js
+++ test/PerseestQuery.js
@@ -3,7 +3,10 @@
 // jcondor (Paolo Lucchesi)
 'use strict'
 const expect = require('expect.js')
+const sinon = require('sinon')
 const Query = require('../lib/PerseestQuery')
+const Params = require('../lib/QueryParameters')
+const ConfigFactory = require('./help/factories').Config
 
 const negativeTestCases = [
   ['an array', ['a', 'b', 'c']],
@@ -56,15 +59,59 @@ describe('A query object', function () {
     })
   })
 
+  // TODO: this.transform should be omittable
   describe('when run', function () {
-    it('should run its hooks')
-    it('should give its name as a parameter')
-    it('should pass the response to the after-hooks')
+    let q, conf;
+    beforeEach(() => {
+      sinon.restore();
+      conf = ConfigFactory.create()
+      conf.pool = { query: sinon.fake.resolves({ rows: [] }) }
+      q = new Query({ name: 'q', generate: () => {}, transform: () => {} })
+    })
+
+    it('should run its hooks', async () => {
+      const beforeHook = sinon.fake()
+      const afterHook = sinon.fake()
+      q.hooks.add('before', beforeHook)
+      q.hooks.add('after', afterHook)
+      await q.run(new Params({ conf }))
+      expect(beforeHook.callCount).to.be(1)
+      expect(afterHook.callCount).to.be(1)
+    })
+
+    it('should give its name as a parameter', async () => {
+      let name;
+      q.hooks.add('before', ({ query }) => name = query)
+      await q.run(new Params({ conf }))
+      expect(name).to.be.ok()
+    })
+
+    it('should pass the response to the after-hooks', async () => {
+      let result;
+      q.hooks.add('after', ({ res }) => result = res)
+      await q.run(new Params({ conf }))
+      expect(result).to.be.ok()
+    })
+
     it('should attempt to return transformed entities by default')
     it('should return the entities transformed with a custom function if given')
+
     describe('should throw an error', function () {
-      specify('on hook failure')
-      specify('on SQL query failure')
+      specify('on hook failure', async () => {
+        q.hooks.add('after', sinon.fake.throws('Failing hook'))
+        await q.run(new Params({ conf }))
+          .then(() => { throw new Error('Running query should have thrown an error') })
+          .catch(() => {})
+      })
+
+      specify('on SQL query failure', async () => {
+        conf.pool.query = sinon.fake.rejects('Failing query')
+        await q.run(new Params({ conf }))
+          .then(() => { throw new Error('Running query should have thrown an error') })
+          .catch(() => {})
+      })
     })
+
+    after(() => sinon.restore())
   })
 })
