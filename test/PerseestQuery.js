6eb23998590f000f870d90a1e31db62a18f17c97
jcondor <paololucchesi98@gmail.com>
2020-05-22 00:23:21 +0200

    Extended query interface and added query types

--- test/PerseestQuery.js
+++ test/PerseestQuery.js
@@ -144,3 +144,143 @@ describe('Perseest.Query', function () {
     after(() => sinon.restore())
   })
 })
+
+
+describe('Perseest.Query.Map', function() {
+  describe('creating', function() {
+    describe('should be successful', function() {
+      specify('with no initial queries', () => new Query.Map())
+      specify('with an iterable collection of valid queries', () =>
+        new Query.Map([ new Query({
+          name: 'q',
+          type: 'boolean',
+          generate: () => {}
+        }) ]))
+    })
+
+    describe('should fail', function() {
+      specify('with a non-iterable collection of valid queries', () =>
+        expect(() => new Query.Map({
+          a: new Query({ name: 'q', type: 'boolean', generate: () => {} }),
+          b: new Query({ name: 'q', type: 'boolean', generate: () => {} })
+        })).to.throwError())
+
+      specify('with an iterable collection of invalid queries', () =>
+        expect(() => new Query.Map([ 'abc', { a: 1 }])).to.throwError())
+    })
+  })
+
+  describe('adding', function() {
+    let map;
+    beforeEach(() => map = new Query.Map())
+
+    describe('should be successful', function() {
+      specify('with valid name/query entry', () => {
+        expect(map.size).to.be(0)
+        map.set('q', new Query({ name: 'q', type: 'boolean', generate: () => {} }))
+        expect(map.size).to.be(1)
+      })
+
+      specify('with valid query instance', () => {
+        expect(map.size).to.be(0)
+        map.add(new Query({ name: 'q', type: 'boolean', generate: () => {} }))
+        expect(map.size).to.be(1)
+      })
+
+      specify('with valid parameters for in-place creation', () => {
+        expect(map.size).to.be(0)
+        map.create({ name: 'q', type: 'boolean', generate: () => {} })
+        expect(map.size).to.be(1)
+      })
+    })
+
+    describe('should fail', function() {
+      const q = new Query({ name: 'q', type: 'boolean', generate: () => {} })
+
+      specify('with invalid name', () => {
+        expect(map.size).to.be(0)
+        expect(() => map.set('@!.-', q)).to.throwError();
+        expect(() => map.set('', q)).to.throwError();
+        expect(() => map.set(undefined, q)).to.throwError();
+        expect(() => map.set(q)).to.throwError();
+        expect(map.size).to.be(0)
+      })
+
+      specify('with invalid query instance, using set()', () => {
+        expect(map.size).to.be(0)
+        expect(() => map.set('q', null)).to.throwError();
+        expect(() => map.set('q', 'abc')).to.throwError();
+        expect(() => map.set('q', { a: 1, b: '2' })).to.throwError();
+        expect(map.size).to.be(0)
+      })
+
+      specify('with invalid query instance, using add()', () => {
+        expect(map.size).to.be(0)
+        expect(() => map.add('boh')).to.throwError()
+        expect(() => map.add({ a: 1, c: 4 })).to.throwError()
+        expect(() => map.add(null)).to.throwError()
+        expect(map.size).to.be(0)
+      })
+
+      specify('with invalid parameters for in-place creation', () => {
+        expect(() => map.create()).to.throwError();
+        expect(() => map.create({ a: 'abc', d: 'def' })).to.throwError();
+      })
+    })
+  })
+})
+
+
+describe('Perseest.Query.TypeMap', function() {
+  describe('creating', function() {
+    describe('should be successful', function() {
+      specify('with no initial types', () => new Query.TypeMap())
+      specify('with an iterable collection of valid type', () =>
+        new Query.TypeMap([ ['dummy', () => 'dummy'] ]))
+    })
+
+    describe('should fail', function() {
+      specify('with a non-iterable collection of valid types', () =>
+        expect(() => new Query.TypeMap({
+          boh: ['dummy1', () => {}],
+          mah: ['dummy2', () => {}],
+        })).to.throwError())
+
+      specify('with an iterable collection of invalid types', () => {
+        expect(() => new Query.TypeMap([ [null, () => {}] ])).to.throwError()
+        expect(() => new Query.TypeMap([ ['type_1', null] ])).to.throwError()
+        expect(() => new Query.TypeMap([ ['t2', { z: 0 }] ])).to.throwError()
+        expect(() => new Query.TypeMap([ [null, null] ])).to.throwError()
+      })
+    })
+  })
+
+  describe('adding entries', function() {
+    let map;
+    beforeEach(() => map = new Query.TypeMap())
+
+    it('should be successful with valid name and transformer', () => {
+      expect(map.size).to.be(0)
+      map.set('dummy', () => 'dummy')
+      expect(map.size).to.be(1)
+    })
+
+    describe('should fail', function() {
+      specify('with invalid name', () => {
+        expect(map.size).to.be(0)
+        expect(() => map.set(null, () => 'dummy')).to.throwError()
+        expect(() => map.set('', () => 'dummy')).to.throwError()
+        expect(map.size).to.be(0)
+      })
+
+      specify('with invalid transformer', () => {
+        expect(map.size).to.be(0)
+        expect(() => map.set('dummy', 'dummy')).to.throwError()
+        expect(() => map.set('dummy', { ret: 'dummy' })).to.throwError()
+        expect(() => map.set('dummy', null)).to.throwError()
+        expect(() => map.set('dummy', undefined)).to.throwError()
+        expect(map.size).to.be(0)
+      })
+    })
+  })
+})
